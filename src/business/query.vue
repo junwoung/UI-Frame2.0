3479d8a8ac501c81a40398ff9d55e862595fe839
wangjun <565831653@qq.com>
2019-03-22 18:51:28 +0800

    提交query插件业务代码

---
+++ src/business/query.vue
@@ -0,0 +1,41 @@
+<!-- created by wangjun on 2019-03-18 -->
+<template>
+  <div>
+    <v-query :query="query" @callback='getOptions'></v-query>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+      query: {
+        src: 'http://172.20.4.230:8080/web-services/dimension/initCondition.do',
+        params: {
+          businessid: '711000',
+          // businessid: '000002',
+          beanid: '',
+          dimension: 1
+        },
+        disable: false
+      }
+    }
+  },
+  methods: {
+    init: function () {
+
+    },
+    getOptions: function (options) {
+      console.log(options)
+    }
+  },
+  mounted () {
+    this.init()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
