a4f5581705edb17deca03dc670e36c601d6495c4
wangjun <565831653@qq.com>
2019-06-05 15:55:34 +0800

    提交tip指令业务

---
+++ src/business/tips.vue
@@ -0,0 +1,30 @@
+<!-- created by wangjun on 2019-05-29 -->
+<template>
+  <div>
+    <span v-tips='"恭喜！你已经成功看到我了"'>查看提示信息</span>
+    <span v-tips='"恭喜！你已经成功看到我了恭喜！你已经成功看到我了恭喜！你已经成功看到我了恭喜！你已经成功看到我了恭喜！你已经成功看到我了"'>查看提示信息</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    init: function () {
+
+    }
+  },
+  mounted () {
+    this.init()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
