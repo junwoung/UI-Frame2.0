b415dce5cb1ed8b743f65b252ee624d93d5c4702
junwoung <565831653@qq.com>
2019-06-21 17:35:01 +0800

    提交提示指令

--- src/directives/resources/tip/tip.js
+++ src/directives/resources/tip/tip.js
@@ -9,19 +9,22 @@ export default (Vue) => {
   require('./tip.css')
   //  缓存多个tips，避免频繁创建节点消耗性能
   //  需要注意的是，避免非必要的提示信息，以免缓存节点过多造成缓存泄露
-  // let doms = []
+  let doms = []
   //  设置tip隐藏定时器
   let timer = null
   Vue.directive('tips', {
     inserted: function (el, bingding) {
       let msg = bingding.value
+      //  绑定该指令的节点，如果定位有特殊要求，则需要以内嵌的形式声明，不然会被替换为relative定位
+      if (el.style.position === '') el.style.position = 'relative'
       init(el, msg)
     }
   })
   //  初始化绑定指令
   function init (el, msg) {
     let dom = createModal(msg)
-    el.parentNode.appendChild(dom)
+    doms.push(el)
+    el.appendChild(dom)
     bindMouseEvents(el, dom)
   }
 
@@ -52,9 +55,43 @@ export default (Vue) => {
 
     //  显示tip信息
     function showTip () {
+      let e = event || window.event
+      //  判断当前操作节点是否是绑定指令节点
+      if (doms.some(dom => dom === e.target)) {
+        //  获取文档节点宽度
+        let docW = document.documentElement.clientWidth
+        //  获取节点现对于浏览器视口位置
+        let rect = e.target.getBoundingClientRect()
+        let [left, width] = []
+        if (docW - rect.left < 150) {
+          //  如果文本节点左边开始位置离视口右边距小于150px，则提示节点向左展示信息
+          left = rect.left - rect.width
+          width = rect.left * 0.8
+          tip.style.left = `${-left}px`
+          //  更改提示节点`三角形`显示位置
+          tip.className = 'v-direc-tip-right'
+        } else {
+          width = (docW - rect.left) * 0.8
+        }
+        //  设置提示节点最大宽度，避免显示错乱或者不美观
+        tip.style['max-width'] = `${width}px`
+        //  计算文本节点大概宽度，按14px计算
+        let text = tip.childNodes[0]
+        let textW = text.length * 14
+        //  如果计算得到的宽度大于提示最大允许宽度，则允许其换行显示
+        if (textW > width) {
+          tip.style['white-space'] = 'normal'
+          tip.style.width = `${width}px`
+          //  文本节点靠右 计算提示左偏移量
+          if (docW - rect.left < 150) {
+            left = width - (rect.width) / 2
+            tip.style.left = `${-left}px`
+          }
+        }
+      }
       tip.style.opacity = 1
       if (timer) return clearTimeout(timer)
-      tip.style.visibility = 'visible'
+      tip.style.display = 'block'
     }
 
     //  隐藏tip信息
@@ -63,10 +100,8 @@ export default (Vue) => {
       tip.style.opacity = 0
       timer = setTimeout(() => {
         timer = null
-        tip.style.visibility = 'hidden'
+        tip.style.display = 'none'
       }, 300)
     }
   }
-
-  //  调整tip显示位置
 }
