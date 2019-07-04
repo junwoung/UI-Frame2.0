6efdaebf3efa3222ca3368dbb3c063816f5efdcb
junwoung <565831653@qq.com>
2019-07-04 10:57:46 +0800

    打开自定义弹窗表单

---
+++ src/business/dialogs/test.vue
@@ -0,0 +1,61 @@
+<!-- created by wangjun on 2019-07-02 -->
+<template lang="html">
+  <div v-if='form'>
+    <div class="form-control">
+      <label class="label label-require">姓名</label>
+      <input class='input' type="text" v-model='form.name'>
+    </div>
+    <div class="form-control">
+      <label class="label label-require">电话</label>
+      <input class='input' type="text" v-model='form.phone'>
+    </div>
+    <div class="form-control">
+      <label class="label label-require">时间</label>
+      <v-time :time='time'></v-time>
+    </div>
+    <div class="form-control">
+      <button class="btn btn-small btn-default" @click='ok'>确定</button>
+      <button class="btn btn-small btn-minor">取消</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    //  这边定义传入的变量
+    parent: {
+      type: Object,
+      default: function () {
+        return {}
+      }
+    },
+    form: {
+      type: Object,
+      default: () => {}
+    },
+    time: {
+      type: Object,
+      default: () => {}
+    },
+    ensure: {
+
+    }
+  },
+  methods: {
+    ok () {
+      if (this.form.name === '') return this.$dialog.msg('姓名不可为空')
+      this.$EventBus.$emit('testFormOk')
+    }
+  },
+  mounted () {
+    console.log(this.time)
+  }
+}
+</script>
+
+<style scoped>
+.form-control {
+  margin-bottom: 20px;
+}
+</style>
