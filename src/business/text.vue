56b38a4619fb95d29c234db0129846dd36de8bc1
junwoung <565831653@qq.com>
2019-07-20 16:27:49 +0800

    修改输入框业务组件

--- src/business/text.vue
+++ src/business/text.vue
@@ -3,29 +3,29 @@
   <div>
     <div class="form-control clearfix">
       <label class="label">输入框使用(属性设置)</label>
-      <v-input v-model="value" :placeholder="'请输入网址'" :icon="'user'" :style="{width: '300px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
-      <v-input v-model="value" :placeholder="'请输入网址'" :icon="'email'" :style="{width: '300px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
-      <v-input :require='true' v-model="value" :prefix="'www.'" :placeholder="'请输入网址'" :icon="'url'" :style="{width: '300px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
-      <v-input v-model="value" :error="error" :placeholder="'请输入网址'" :icon="'password'" :style="{width: '300px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
+      <v-input v-model="value" :placeholder="'请输入网址'" :icon="'user'" :style="{width: '260px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
+      <v-input v-model="value" :placeholder="'请输入网址'" :icon="'email'" :style="{width: '260px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
+      <v-input :require='true' v-model="value" :prefix="'www.'" :placeholder="'请输入网址'" :icon="'url'" :style="{width: '260px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
+      <v-input v-model="value" :error="error" :placeholder="'请输入网址'" :icon="'password'" :style="{width: '260px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
       <v-input v-model="value" :tail="'.com'" :icon="'number'"></v-input>
-      <v-input v-model="value" :prefix="'www.'" :tail="'.com'" :placeholder="'请输入网址'" :icon="'search'" :style="{width: '300px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
-      <v-input v-model="value" :placeholder="'请输入网址'" :icon="'date'" :style="{width: '300px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
-      <v-input v-model="value" :placeholder="'请输入网址'" :icon="'time'" :style="{width: '300px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
-      <v-input v-model="value" :placeholder="'请输入网址'" :icon="'tel'" :style="{width: '300px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
-      <v-input v-model="value" :placeholder="'请输入网址'" :icon="'phone'" :style="{width: '300px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
-      <v-input v-model="value" :placeholder="'请输入网址'" :icon="'addr'" :style="{width: '300px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
-      <v-input v-model="value" :placeholder="'请输入网址'" :icon="'dropdown'" :style="{width: '300px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
-      <v-input readonly v-model="value" :placeholder="'请输入网址'" :icon="'dropdown'" :style="{width: '300px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
+      <v-input v-model="value" :prefix="'www.'" :tail="'.com'" :placeholder="'请输入网址'" :icon="'search'" :style="{width: '260px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
+      <v-input v-model="value" :placeholder="'请输入网址'" :icon="'date'" :style="{width: '260px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
+      <v-input v-model="value" :placeholder="'请输入网址'" :icon="'time'" :style="{width: '260px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
+      <v-input v-model="value" :placeholder="'请输入网址'" :icon="'tel'" :style="{width: '260px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
+      <v-input v-model="value" :placeholder="'请输入网址'" :icon="'phone'" :style="{width: '260px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
+      <v-input v-model="value" :placeholder="'请输入网址'" :icon="'addr'" :style="{width: '260px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
+      <v-input v-model="value" :placeholder="'请输入网址'" :icon="'dropdown'" :style="{width: '260px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
+      <v-input readonly v-model="value" :placeholder="'请输入网址'" :icon="'dropdown'" :style="{width: '260px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
     </div>
     <div class="form-control clearfix">
       <label class="label">输入框使用(slot自定义)</label>
-      <v-input v-model="value" type="number" :min="20" :max="500" :require='true' :icon="'number'" :style="{width: '300px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}">
+      <v-input v-model="value" type="number" :min="20" :max="500" :require='true' :icon="'number'" :style="{width: '260px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}">
         <em slot="icon" class="tips">500</em>
       </v-input>
-      <v-input disabled v-model="hello" :style="{width: '300px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}">
+      <v-input disabled v-model="hello" :style="{width: '260px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}">
         <em slot="icon" class="tips">128字</em>
       </v-input>
-      <v-input :require='true' maxlength="4" :minLen='4' v-model="value" :style="{width: '300px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}">
+      <v-input :require='true' maxlength="4" :minLen='4' v-model="value" :style="{width: '260px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}">
         <em slot="icon" class="tips">128字</em>
       </v-input>
     </div>
@@ -33,11 +33,11 @@
     <div style="margin-top: 30px;" class="form-control clearfix">
       <label class="label">使用</label>
       <textarea disabled class="textarea" style="width: 400px;height: 160px;">
-        <v-input :require='true' v-model="value" :prefix="'www.'" :placeholder="'请输入网址'" :icon="'url'" :style="{width: '300px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
+        <v-input :require='true' v-model="value" :prefix="'www.'" :placeholder="'请输入网址'" :icon="'url'" :style="{width: '260px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}"></v-input>
         <!-- 对应第一栏第6个输入框 -->
       </textarea>
       <textarea disabled class="textarea exp2" style="width: 400px;height: 160px;">
-        <v-input v-model="value" type="number" :min="20" :max="500" :require='true' :icon="'number'" :style="{width: '300px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}">
+        <v-input v-model="value" type="number" :min="20" :max="500" :require='true' :icon="'number'" :style="{width: '260px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}">
           <em slot="icon" class="tips">500</em>
         </v-input>
         <!-- 对应第二栏第1个输入框 -->
@@ -87,7 +87,7 @@ export default {
         {arg: 'v-model', type: 'String', desc: '接收一个用于获取值的参数名', must: 'false', exp: 'form.username', default: 'null'},
         {arg: ':placeholder', type: 'String', desc: '输入框底字', must: 'false', exp: '请输入用户名', default: 'null'},
         {arg: ':icon', type: 'String', desc: '输入框底图呈现（可以用slot重新设定），内部预设样式：user/email/url/password/number/search/date/time/phone/tel/addr/dropdown', must: 'false', exp: 'user', default: 'null'},
-        {arg: ':style', type: 'Object', desc: '定义样式，暂支持定义输入框宽高、圆角、边框颜色', must: 'false', exp: `{width: '300px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}`, default: 'null'},
+        {arg: ':style', type: 'Object', desc: '定义样式，暂支持定义输入框宽高、圆角、边框颜色', must: 'false', exp: `{width: '260px', height: '40px', 'border-radius': '4px', 'border-color': '#ddd'}`, default: 'null'},
         {arg: ':prefix', type: 'String', desc: '设定输入框前缀', must: 'false', exp: 'www.', default: 'null'},
         {arg: ':tail', type: 'String', desc: '设定输入框尾缀', must: 'false', exp: '.com', default: 'null'},
         {arg: ':require', type: 'Boolean', desc: '输入框是否必填（未填写时，给予报错提示）', must: 'false', exp: 'true', default: 'false'},
@@ -108,15 +108,15 @@ export default {
       let wh = window.innerHeight
       let distance = wh * 0.5 - expRect.top - expRect.height * 0.5
       scroll()
-      setTimeout(() => bling(), 300)
-      setTimeout(() => clear(), 1300)
+      setTimeout(() => bling(), 260)
+      setTimeout(() => clear(), 1260)
       setTimeout(() => bling(), 1800)
       setTimeout(() => clear(), 2800)
-      setTimeout(() => bling(), 3300)
-      setTimeout(() => clear(), 4300)
+      setTimeout(() => bling(), 3260)
+      setTimeout(() => clear(), 4260)
       function scroll () {
         let timeGap = 16.7
-        let totalTime = 300
+        let totalTime = 260
         let steps = Math.ceil(totalTime / timeGap)
         let step = distance / steps
         let i = 0
