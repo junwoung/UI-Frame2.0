0b1e9c305bc8cda823ef403a9e3f10c6caa5e0c9
junwoung <565831653@qq.com>
2019-07-27 17:52:50 +0800

    集中注册dropdown 和 drop-option组件

---
+++ src/components/vDropdown/inject.js
@@ -0,0 +1,11 @@
+/**
+* @author: wangjun
+* @date: 2019-07-25 21:37:04
+* @desc: 集中注册dropdown和dropoption组件
+*/
+
+import Vue from 'vue'
+import {dropdown, dropOption} from './dropdown.js'
+
+Vue.use(dropdown)
+Vue.use(dropOption)
